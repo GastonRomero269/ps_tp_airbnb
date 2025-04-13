21db9e1d9986a47180791a9e6aee65e83188e88b
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 15:01:10 -0300

    Property Service Implementation

---
+++ src/main/java/com/unla/ps_tp_airbnb/service/PropertyServiceImpl.java
@@ -0,0 +1,55 @@
+package com.unla.ps_tp_airbnb.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.unla.ps_tp_airbnb.model.Property;
+import com.unla.ps_tp_airbnb.repository.PropertyRepository;
+import com.unla.ps_tp_airbnb.serviceInterface.PropertyService;
+
+@Service
+public class PropertyServiceImpl implements PropertyService {
+
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    public List<Property> findAll() {
+        return propertyRepository.findAll();
+    }
+
+    public Optional<Property> findById(Long id) {
+        return propertyRepository.findById(id);
+    }
+
+    public Property save(Property property) {
+        return propertyRepository.save(property);
+    }
+
+    public void delete(Long id) {
+        propertyRepository.deleteById(id);
+    }
+
+	@Override
+	public List<Property> findByLocationContainingIgnoreCase(String query) {
+		return propertyRepository.findByLocationContainingIgnoreCase(query);
+	}
+
+	@Override
+	public List<Property> findByRatingGreaterThan(double rating) {
+		return propertyRepository.findByRatingGreaterThan(rating);
+	}
+
+	@Override
+	public List<Property> findByTitleContainingIgnoreCase(String title) {
+		return propertyRepository.findByTitleContainingIgnoreCase(title);
+	}
+
+	@Override
+	public List<Property> findByHostId(Long id) {
+		return propertyRepository.findByHostId(id);
+	}
+}
+
