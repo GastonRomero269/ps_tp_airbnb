e0713d14cb11587c71fdca45eb840c7e0c7a3d2d
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:56:54 -0300

    Type Guests Repository

---
+++ src/main/java/com/unla/ps_tp_airbnb/repository/TypeGuestsRepository.java
@@ -0,0 +1,9 @@
+package com.unla.ps_tp_airbnb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.ps_tp_airbnb.model.TypeGuests;
+
+public interface TypeGuestsRepository extends JpaRepository<TypeGuests, Long>{
+
+}
