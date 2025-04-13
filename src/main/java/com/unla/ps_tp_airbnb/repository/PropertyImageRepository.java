b7935b556119fa2ba0ca5366f05fe15812cea71c
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:55:17 -0300

    Property Image Repository

---
+++ src/main/java/com/unla/ps_tp_airbnb/repository/PropertyImageRepository.java
@@ -0,0 +1,7 @@
+package com.unla.ps_tp_airbnb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.ps_tp_airbnb.model.PropertyImage;
+
+public interface PropertyImageRepository extends JpaRepository<PropertyImage, Long> {}
