25e562058eef1258ed479ea9a3049702cd58800b
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:59:00 -0300

    Review Service Interface

---
+++ src/main/java/com/unla/ps_tp_airbnb/serviceInterface/ReviewService.java
@@ -0,0 +1,14 @@
+package com.unla.ps_tp_airbnb.serviceInterface;
+
+import java.util.List;
+
+import com.unla.ps_tp_airbnb.model.Review;
+
+public interface ReviewService {
+    List<Review> findAll();
+    Review findById(Long id);
+    Review save(Review review);
+    void delete(Long id);
+	List<Review> findByPropertyId(Long id);
+	public double calculateAverageRatingByPropertyId(Long propertyId);
+}
