7ffbdf22fbd298f182ddfc32e8a55e349db552cb
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:57:13 -0300

    Message Repository

---
+++ src/main/java/com/unla/ps_tp_airbnb/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.unla.ps_tp_airbnb.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.ps_tp_airbnb.model.Message;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+	List<Message> findByReceiverIdOrSenderId(Long receiverId, Long senderId);
+}
