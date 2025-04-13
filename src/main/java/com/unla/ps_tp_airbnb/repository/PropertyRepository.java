52ca5a1bfb8244bfe0618bb54bccd6fcacc86766
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:55:35 -0300

    Property Repository

---
+++ src/main/java/com/unla/ps_tp_airbnb/repository/PropertyRepository.java
@@ -0,0 +1,14 @@
+package com.unla.ps_tp_airbnb.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.ps_tp_airbnb.model.Property;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+	List<Property> findByLocationContainingIgnoreCase(String location);
+	List<Property> findByRatingGreaterThan(double rating);
+	List<Property> findByTitleContainingIgnoreCase(String title);
+	List<Property> findByHostId(Long id);
+}
