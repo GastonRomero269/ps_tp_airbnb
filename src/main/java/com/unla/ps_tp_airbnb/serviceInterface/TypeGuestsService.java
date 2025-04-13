1842ee86432f64c9b2f73149e8a19f126a52d2a7
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:59:15 -0300

    Type Guests Service Interface

---
+++ src/main/java/com/unla/ps_tp_airbnb/serviceInterface/TypeGuestsService.java
@@ -0,0 +1,13 @@
+package com.unla.ps_tp_airbnb.serviceInterface;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.unla.ps_tp_airbnb.model.TypeGuests;
+
+public interface TypeGuestsService {
+    TypeGuests save(TypeGuests typeGuest);
+    Optional<TypeGuests> findById(Long id);
+    List<TypeGuests> findAll();
+    void delete(Long id);
+}
