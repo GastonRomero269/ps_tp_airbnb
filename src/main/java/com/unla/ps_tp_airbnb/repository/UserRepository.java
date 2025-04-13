fcb9b46b8f3df79a9e8fd4068a867f33f33ca102
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-04-13 14:54:57 -0300

    User Repository

---
+++ src/main/java/com/unla/ps_tp_airbnb/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.unla.ps_tp_airbnb.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.ps_tp_airbnb.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndPasswordHash(String email, String passwordHash);
+    Optional<User> findByEmail(String email);
+}
